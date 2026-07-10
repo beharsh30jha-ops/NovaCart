df64931224f867c613ae33172b59cc0752fc0306
beharsh30jha-ops <beharsh30jha@gmail.com>
2026-07-10 22:02:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+// NovaCart JavaScript
+
+console.log("NovaCart Loaded Successfully!");
+
+const button = document.querySelector(".hero button");
+
+button.addEventListener("click", function () {
+    alert("🛒 Welcome to NovaCart!\nShopping features are coming soon.");
+});
